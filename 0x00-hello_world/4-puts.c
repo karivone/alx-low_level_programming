5d42549081e0a41b5815b9b7d375eafc960b991b
karivone <karimiyvonne19@gmail.com>
2022-09-08 09:08:45 -0700

    prints out a statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,15 @@
 
 
 
-/**
+/*
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
  *
- *    * Return: Always 0 (Success)
  *
- *     */
+ * Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 
