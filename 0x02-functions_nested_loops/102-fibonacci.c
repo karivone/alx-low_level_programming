f62a77d5494d7b4972e090ed4e22e99908fb58f4
karivone <karimiyvonne19@gmail.com>
2022-09-14 05:28:12 -0700

    Prints the first fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,14 +14,14 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li %li ", a, b);
+		printf("%li , %li ", a, b);
 		a += b;
 		b += a;
 	}
 	if (countto % 2 == 1)
 		printf("%li", a);
 
-	printf("%li\n");
+	printf("\n");
 
 	return (0);
 }
