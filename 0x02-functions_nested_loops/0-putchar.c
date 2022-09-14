906710535f71b8f9a445dc9d718b59a0143727a3
karivone <karimiyvonne19@gmail.com>
2022-09-14 01:30:00 -0700

    The firs task on nested  loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+
+		_putchar('-');
+		_putchar('P');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
+
+}
