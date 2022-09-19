1d32c218e1213e21db4acd68b378301c639afb98
karivone <karimiyvonne19@gmail.com>
2022-09-19 04:25:27 -0700

    function that copies

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
  * @src: The source string to copy.
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
