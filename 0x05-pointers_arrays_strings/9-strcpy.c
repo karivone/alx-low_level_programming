f34cfc3b716631435128143000f392eb777ab41f
karivone <karimiyvonne19@gmail.com>
2022-09-19 04:40:14 -0700

    function that copies

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,9 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = -0;
+	int index = 0;
 
-	while (src[index] != '\0')
+	while (*src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
