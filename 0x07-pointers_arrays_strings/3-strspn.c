296b60c9669022d648a0420e9edbee9d41d39f16
karivone <55494837+karivone@users.noreply.github.com>
2022-09-26 17:06:31 +0300

    return the number of occurent of a string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,12 +14,14 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (s[i] == accept[j])
+		for (j = 0; accept[j] > '\0'; j++)
 		{
+			if (s[i] == accept[j])
+			{
 			cmpt++;
 			break;
+			}
 		}
-	}
 	if (accept[j] == '\0')
 	{
 		break;
