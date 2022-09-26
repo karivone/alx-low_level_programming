51c8cfee17901f614afc2c9145933fe404e8409d
karivone <55494837+karivone@users.noreply.github.com>
2022-09-26 17:34:48 +0300

    function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strstr - function that finds the first occurence of
+  * substring 'needle'
+  * @haystack: string
+  * @needle: pointer
+  * Return: pointer
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] > '\0'; i++)
+	{
+		for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+		{
+			if (haystack[j] != needle[j - i])
+			{
+				break;
+			}
+		}
+		if (needle[j - i] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
