d779e95504410e680a79066af420714790ca2972
karivone <55494837+karivone@users.noreply.github.com>
2022-09-28 13:12:06 +0300

    prints a string in reverse'

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _print_rev_recursion - prinnts a string in reverse
+  *
+  * @s: the string to print
+  *
+  * Return: void
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
