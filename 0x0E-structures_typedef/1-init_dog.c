f07b59651f1ba59d331cea229160af78088e10e8
karivone <karimiyvonne19@gmail.com>
2022-10-11 06:31:05 -0700

    structs

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)/* validate if d initiated correctly */
-		return;
-
+	if (d != NULL)/* validate if d initiated correctly */
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
 }
