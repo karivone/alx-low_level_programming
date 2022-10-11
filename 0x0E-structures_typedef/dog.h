737ef8e4cebc80ee83fb41024179561bb0f696d8
karivone <karimiyvonne19@gmail.com>
2022-10-11 06:21:55 -0700

    structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
