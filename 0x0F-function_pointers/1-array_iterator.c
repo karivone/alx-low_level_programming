73b5983c6ddecc19faefd91b22b29febf9a7b8df
karivone <karimiyvonne19@gmail.com>
2022-10-13 12:33:34 -0700

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,12 +5,13 @@
  * @size: the size of an array
  * @action: a pointer to the function
  */
-void array_iterator(int *array, size_t size, void (*action)(int){
+void array_iterator(int *array, size_t size, void (*action)(int)
+{
 	size_t i; /*match data type*/
 
 	if (array != NULL && action != NULL)
-	
-	for (i = 0; i < size; i++);
+
+	for (i = 0; i < size; i++)
 
 	action(*(array + i));
 }
