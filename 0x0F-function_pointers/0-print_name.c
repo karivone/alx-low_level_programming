36ecdec1ce2966dcf70302791d86bf8e0a6d754d
karivone <karimiyvonne19@gmail.com>
2022-10-14 14:12:53 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- * print_name - prints name
+ * print_name - prints namee
  * @name: name of the user
  * @f: pointer to the function
  * Return: Nothing
