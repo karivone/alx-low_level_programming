793e2416cf5dc36592e1a1c86a09965e3c885571
karivone <karimiyvonne19@gmail.com>
2022-10-24 04:50:56 -0700

    Singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * _strlen - find string length
+ * @str: string
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
+ * add_node - add node to beginningof linked list
+ * @head: linked list
+ * @str: data for new node
+ * Return: address of new elements, or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node; /* creates a new node*/
+
+	if (str == NULL) /* creates new node */
+		return (NULL);
+	if (strdup(str) == NULL) /* check if strdup malloc errored*/
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t)); /*malloc for new node */
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str); /* set node values */
+	new_node->len = _strlen(str);
+
+	if (head == NULL) /* set what new node points to first */
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	*head = new_node; /*set head to point to new node*/
+
+	return (new_node);
+}
