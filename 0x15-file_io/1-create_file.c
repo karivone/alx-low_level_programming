9f9a2f4d4a539487fd3a3a1ea957eef9884c1245
karivone <karimiyvonne19@gmail.com>
2022-11-02 15:58:14 -0700

    file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _strlen - find the length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int n_wrote;
+
+	if (!filename)
+		return (-1);
+
+	/* create with permissions if file doesn't exist, else truncate */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	/* if nothing to writw, just return newly created files */
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
