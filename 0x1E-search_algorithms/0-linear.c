34e22ce9733738f7619a7e6b8b974a3ae5407f00
karivone <karimiyvonne19@gmail.com>
2023-04-13 06:50:20 -0700

    search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,18 +6,15 @@
  * @value: value to search for
  * Return: index at which value's found
  */
- 
 int linear_search(const int *array, size_t size, int value)
 {
-      if (!array)
-         return (-1);
-
-      for (size_t i = 0; i < size; i++)
-      {
-          printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-          if (array[i] == value)
-              return (i);
-      }
-
+	if (!array)
+		return (-1);
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,48 +1,47 @@
 #include "search_algos.h"
-
 /**
-* print_arr - helper func to print array everytime array is halved
-* @array: array
-* @l: left index of original array
-* @r: right index of original array
-*/
+ * print_arr - helper func to print array everytime array is halved
+ * @array: array
+ * @l: left index of original array
+ * @r: right index of original array
+ */
 void print_arr(const int *array, unsigned int l, unsigned int r)
 {
 	printf("Searching in array: ");
 	for (unsigned int i = l; i <= r; i++)
-	{
-		if (i != r)
-		    printf("%d, ", array[i]);
-		else
-		    printf("%d\n", array[i]);
-	}
+		{
+			if (i != r)
+				printf("%d, ", array[i]);
+			else
+				printf("%d\n", array[i]);
+		}
 }
+
 /**
-* binary_search - search mids
-* @array: given array of ints
-* @size: size of array
-* @value: value to search for
-* Return: index at which value's found
-*/
+ * binary_search - search mids
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
 int binary_search(const int *array, unsigned int size, int value)
 {
-    unsigned int mid = 0;
-    unsigned int l = 0;
-    unsigned int r = size - 1;
+	unsigned int mid = 0;
+	unsigned int l = 0;
+	unsigned int r = size - 1;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
+	do {
+		print_arr(array, l, r);
+		mid = (l + r) / 2;
 
-    do {
-        print_arr(array, l, r);
-        mid = (l + r) / 2;
-        if (array[mid] == value)
-             return (mid);
-        else if (array[mid] > value)
-             r = mid - 1;
-        else
-             l = mid + 1;
-    } while (l <= r);
-
-    return (-1);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			r = mid - 1;
+		else
+			l = mid + 1;
+		} while (l <= r);
+	return (-1);
 }
